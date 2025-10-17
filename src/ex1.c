874f86ee460e4ca8a88877722580568eb9e8782c
Kauê Lima <kauelima557@gmail.com>
2025-10-17 19:04:42 +0000

    inicio do For

--- src/ex1.c
+++ src/ex1.c
@@ -7,12 +7,20 @@
 
 int main() {
     char entrada[MAX_TAMANHO];
+    int cont = 0; 
 
     printf("Digite a sequencia de letras: ");
     fgets(entrada, sizeof(entrada), stdin);
     entrada[strcspn(entrada, "\n")] = '\0'; // remove o \n lido pelo fgets
 
-    printf("%s", entrada); // teste para ver se esta lendo
+    printf("%s", entrada); // teste para ver se esta lendo coretamente
+
+    // LEITURA DE CADA CARACTERE + CONTAGEM
+    for(int i = 0; i < sizeof(entrada); i++){
+        while(entrada[i] = entrada[i+1]){ // enquanto a "letra lida" for IGUAL a proxima letra
+            cont++;
+        }
+    }
 
     return 0;
 }

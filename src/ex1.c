c571207718aa77fd0bf945e62068f377d6ec0faa
Kauê Lima <kauelima557@gmail.com>
2025-10-17 15:10:42 +0000

    ex1 começo

--- src/ex1.c
+++ src/ex1.c
@@ -1,1 +1,18 @@
-git s
+// Escreva em C o algoritmo ex1 que compacta elementos contíguos repetidos
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_TAMANHO 100
+
+int main() {
+    char entrada[MAX_TAMANHO];
+
+    printf("Digite a sequencia de letras: ");
+    fgets(entrada, sizeof(entrada), stdin);
+    entrada[strcspn(entrada, "\n")] = '\0'; // remove o \n lido pelo fgets
+
+    printf("%s", entrada); // teste para ver se esta lendo
+
+    return 0;
+}
